838f12410919bc32cfe495c20a197556248fa57e
wwhai <751957846@qq.com>
2019-05-16 11:15:02 +0800

    demo优化了一下

--- src/main/java/com/shining/Main.java
+++ src/main/java/com/shining/Main.java
@@ -7,7 +7,7 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class Main {
     public static void main(String[] args) {
-        ServerMqttClient serverMqttClient = new ServerMqttClient("localhost",
+        ServerMqttClient serverMqttClient = new ServerMqttClient("39.96.58.211",
                 1883,
                 "clientId",
                 "username",
@@ -16,6 +16,7 @@ public class Main {
             @Override
             public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
                 super.messageArrived(topic, mqttMessage);
+                System.out.println(mqttMessage);
             }
         });
         if (isConnect) {
@@ -23,23 +24,37 @@ public class Main {
             /**
              * 订阅
              */
-            serverMqttClient.subscribe("topic", 2);
+            serverMqttClient.subscribe("zyzs", 2);
             /**
              * 发布 Message
              */
             MqttMessage mqttMessage = new MqttMessage();
             mqttMessage.setPayload(new byte[]{70, 85, 67, 75});
-            serverMqttClient.publish("topic", new MqttMessage());
+            //serverMqttClient.publish("zyzs", new MqttMessage());
             /**
              * 发布JSON
+             *
+             Madison 2019/5/16 10:52:04
+             {
+             "action": "1",
+             "msgid": "20190221095350622453353",
+             "type": "1",
+             "price": "1"
+             }
              */
             JSONObject jsonObject = new JSONObject();
-            jsonObject.put("name", "wwhai");
-            serverMqttClient.publishJson("topic", jsonObject);
+            jsonObject.put("action", "1");
+            jsonObject.put("msgid", System.currentTimeMillis());
+            jsonObject.put("type", "1");
+            jsonObject.put("price", "1");
+
+            serverMqttClient.publishJson("zyzs", jsonObject);
             /**
              * 发布Hex
              */
-            serverMqttClient.publishHex("topic", new byte[]{97, 98, 99, 100});
+           // serverMqttClient.publishHex("zyzs", new byte[]{97, 98, 99, 100});
+        }else {
+            System.out.println("连接失败");
         }
 
     }
